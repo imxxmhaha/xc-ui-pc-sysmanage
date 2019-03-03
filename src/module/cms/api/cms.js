8ead7e51ee862f7f4a2cc052f0b4246f3967e97f
gituser <gituser@192.168.156.1>
2019-03-04 00:42:25 +0800

    Initial commit

---
+++ src/module/cms/api/cms.js
@@ -0,0 +1,16 @@
+import http from './../../../base/api/public'
+import querystring from 'querystring'
+let sysConfig = require('@/../config/sysConfig')
+let apiUrl = sysConfig.xcApiUrlPre;
+
+//页面查询
+export const page_list = (page,size,params) =>{
+  //将params对象数据拼装成key/value串
+  let queryString =  querystring.stringify(params);
+  //请求服务端的页面查询接口
+  return http.requestQuickGet(apiUrl+'/cms/page/list/'+page+'/'+size+'?'+queryString);
+}
+
+
+
+
